e0f0972c4390c73c051e55d58331373ded8d2bf6
CECurtiss <curtissclinton@gmail.com>
2022-10-14 21:46:56 -0500

    added textarea to scheduler and styled hourly schedule. added button for saving text area

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 currentDay = moment().format("dddd, MMMM DD YYYY");
 $('#currentDay').text(currentDay);
 
-jQuery('.container').datetimepicker();
+var containerEl = $('.container');
+
+containerEl.css('border', 'rgb(243,195,094) 5 px, solid');
