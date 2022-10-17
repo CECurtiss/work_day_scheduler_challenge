957ef6bbfe688d5182ca0da3727ce700a5b8e37d
CECurtiss <curtissclinton@gmail.com>
2022-10-17 14:43:24 -0500

    working on local storage issue. submitting for up to date info for Ask BCS

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 currentDay = moment().format("dddd, MMMM DD YYYY");
 $('#currentDay').text(currentDay);
-var savedToDo = $('#savedtodo');
 var saveBtn = $('.saveBtn');
 
-localStorage.getItem(savedToDo).value;
+$('.saveBtn').click(function() {
+    localStorage.setItem("savedtodo",JSON.stringify( $('#savedtodo').val()
+    ));
 
-saveBtn.addEventListener("click",function() {
-    localStorage.setItem(savedToDo).value;
-})
+});
+
+
+JSON.parse(localStorage.getItem('savedtodo'));
