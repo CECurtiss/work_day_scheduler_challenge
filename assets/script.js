9575e6850933de9884bc5700dfefc4703bf2f1c8
CECurtiss <curtissclinton@gmail.com>
2022-10-17 18:20:22 -0500

    added method to save in local storage

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,15 @@ var saveBtn = $('.saveBtn');
 
 
 $('.saveBtn').click(function() {
-    var savedText = $(this).siblings('.description').val();
-    console.log(savedText);
-    
+    var value = $(this).siblings('.description').val();
+    console.log(value);
+    var time = $(this).data("time");
+    console.log(time);
+    localStorage.setItem(value,time);
 
 });
 
+localStorage.getItem(value);
 
 
 currentTime= moment().hour();
