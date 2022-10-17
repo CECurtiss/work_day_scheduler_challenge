ad706a052377b508bb335c79f1a6447a1b9884ff
CECurtiss <curtissclinton@gmail.com>
2022-10-17 15:48:38 -0500

    added unique class names to each time so I could reference each textarea for local storage

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,15 @@ currentDay = moment().format("dddd, MMMM DD YYYY");
 $('#currentDay').text(currentDay);
 var saveBtn = $('.saveBtn');
 
+
+
 $('.saveBtn').click(function() {
-    localStorage.setItem("savedtodo",JSON.stringify( $('#savedtodo').val()
-    ));
+    // localStorage.setItem("savedtodo",JSON.stringify( $('#savedtodo').val()
+    // 
 
 });
 
 
-JSON.parse(localStorage.getItem('savedtodo'));
 
 currentTime= moment().hour();
 console.log(currentTime);
