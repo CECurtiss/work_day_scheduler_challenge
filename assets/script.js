ac536a927f517a9a1843cdcf6793ed0c91800588
CECurtiss <curtissclinton@gmail.com>
2022-10-19 21:13:40 -0500

    figured out how to make text stay on page when reloaded

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,26 @@ $('#currentDay').text(currentDay);
 var saveBtn = $('.saveBtn');
 
 
+$('#nine').val(localStorage.getItem('9'));
+$('#ten').val(localStorage.getItem('10'));
+$('#eleven').val(localStorage.getItem('11'));
+$('#twelve').val(localStorage.getItem('12'));
+$('#one').val(localStorage.getItem('1'));
+$('#two').val(localStorage.getItem('2'));
+$('#three').val(localStorage.getItem('3'));
+$('#four').val(localStorage.getItem('4'));
+$('#five').val(localStorage.getItem('5'));
 
 $('.saveBtn').click(function() {
     var value = $(this).siblings('.description').val();
     console.log(value);
     var time = $(this).data("time");
     console.log(time);
-    localStorage.setItem(value,time);
-
+    localStorage.setItem(time,value);
+    
 });
 
-localStorage.getItem(value);
 
 
 currentTime= moment().hour();
 console.log(currentTime);
-
